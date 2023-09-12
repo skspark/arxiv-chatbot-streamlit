686f042addd21f5505eafc0a350123b615975a6f
skspark <wondershine.one@gmail.com>
2023-09-12 23:40:45 +0900

    add simple tc on parser

--- tests/unit_tests/test_parser.py
+++ tests/unit_tests/test_parser.py
@@ -1,37 +1,24 @@
 import unittest
 
-import pytest
-from typing import List
-
-from arxiv_paper_chatbot.parser import PaperParser  # Import the class containing _parse_title
+from arxiv_paper_chatbot.parser import PaperParser
 from py_pdf_parser.loaders import PDFDocument, load_file
+from arxiv_paper_chatbot.paper import Paper
+
+import unittest
 
 
 class TestParseTitle(unittest.TestCase):
-    def test_parse_title_with_valid_data(self):
-        doc = PDFDocument(pages={}) # TODO
-        result = PaperParser._parse_title(doc)
-        self.assertEqual(result, 'This is a title')
+    def setUp(self):
+        self.parser = PaperParser()
 
-    def test_parse_title_with_empty_data(self):
-        empty_doc = PDFDocument({})
-        result = PaperParser._parse_title(empty_doc)
-        self.assertEqual(result, "")
+    def test_parse_with_empty_document(self):
+        doc = PDFDocument(pages={})
+        result = self.parser.parse(doc)
+        self.assertIsNone(result)
 
-    def test_parse_title_with_no_valid_title(self):
-        # Create a PDFDocument with no valid title
-        sample_data = {
-            'pages': [
-                {
-                    'elements': [
-                        {'text': 'Short text', 'font_size': 8},
-                        {'text': 'Another short text', 'font_size': 9},
-                    ]
-                }
-            ]
-        }
-        doc = PDFDocument(pages={}) # TODO
-
-        # Call the _parse_title method and assert the result
-        result = PaperParser._parse_title(doc)
-        self.assertEqual(result, "")
+    def test_parse_with_valid_document(self):
+        doc = load_file("tests/resources/test.pdf")
+        result = self.parser.parse(doc)
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, Paper)
+        self.assertTrue("Lorem ipsum" in result.raw)
